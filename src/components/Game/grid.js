b326949c97ccd19519942df88dfde40bcc706bd4
taty2010 <shydalys123@gmail.com>
2020-10-22 23:54:44 -0500

    redo

--- src/components/Game/grid.js
+++ src/components/Game/grid.js
@@ -1,55 +1,107 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
 import '../../index.scss'
-import Boxes from "./box";
-import {Stage, Layer,Rect,Text,Circle,Line} from 'react-konva';
-
+import produce from 'immer';
 const Grid = () => {
-  const[tBox, setTBox] = useState(false)
-  const [cell, setCell] = useState([])
-  const[row, setRow] = useState(20)
-  const[col, setcol] = useState(20)
-  const rows = [];
-  const columns = [];
-  const boxesr = []
 
-  for(let r = 0; r < row; r++){
-    rows.push(r)
+  const[row, setRow] = useState(50)
+  const[col, setcol] = useState(50)
+  const [operations, setOperations] = useState([
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0]
+  ])
+
+  const EmptyGrid = () => {
+    const rows = [];
+    for(let r = 0; r < row; r++){
+      rows.push(Array.from(Array(col), () => 0));
+    }
+    return rows
   }
-  // console.log(rows)
-  for(let c = 0; c < col; c++){
-    columns.push(c)
-  }
 
-  const squareClick = (r, c) => {
-    console.log('this square was clicked')
-    setTBox(!tBox)
-  }
+  const [grid, setGrid] = useState(() => {
+    return EmptyGrid()
+  })
 
-  // const handleChange = (e) => {
-  //   setCell({...cell, [e.target.name]: e.target.value})
-  // }
+  const [running, setRunning] = useState(false);
 
-console.log(tBox)
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+      if(!runningRef.current) {
+        return;
+      }
+
+      setGrid((g) => {
+        return produce(g, gridCopy => {
+        for (let i = 0; i < row; i++){
+          for (let k = 0; k < col; k++){
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < row && newK >= 0 && newK < col){
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3){
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3){
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+    });
+  });
+
+    setTimeout(runSimulation, 100);
+  }, []);
+
   return(
+    <>
+    <button onClick={() =>{
+      setRunning(!running);
+      if (!running){
+      runningRef.current = true;
+      runSimulation()
+    }
+    }}>
+        {running ? 'Stop' : 'Start'}
+    </button>
+    <button onClick={() =>{
+      setGrid(EmptyGrid())
+    }}>
+        Clear
+    </button>
+    <button onClick={() =>{
+      setGrid(EmptyGrid())
+    }}>
+        Random
+    </button>
     <div id='container'>
-      {rows.map((r,i) => (
-        columns.map((c,i) => (
-          <Boxes
-          tBox={tBox}
-          setTBox={setTBox} 
-          columns={c}
-          rows={r}
-          index={i}
-          squareClick={squareClick}
-          // className={tBox ? 'alive' : 'dead'}
-          key ={i}
-          // value={r + c}
-          // onClick = {(e) => {console.log('row',r, 'colum', c);squareClick(e.target.value)}}
-         ></Boxes>
-        ))
-      ))}
-      {/* {boxesr} */}
+      {grid.map((rows, i) => 
+        rows.map((col, k) => 
+          <div
+          onClick={() => {
+            const newGrid = produce(grid, gridCopy => {
+                gridCopy[i][k] = grid[i][k] ? 0 : 1;
+            });
+            setGrid(newGrid)
+          }}
+          key={`${i}-${k}`}
+          className={ `boxes ${grid[i][k] ? 'alive': 'dead'}`}
+         ></div>
+         )
+      )}
     </div>
+    </>
   )
 }
 
--- src/components/intro/home.js
+++ src/components/intro/home.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Grid from '../Game/grid';
 const Home = () => {
 
+  const[interval, setInterval] = useState(100)
+  const[running, setRunning] = useState(false)
+console.log(running)
   return(
     <div>
       <h1>Conways Game of Life</h1>
       <p>This will break down what the game is about blah blah blah</p>
-      <button>start</button>
+      <button onClick= {() => setRunning(!running)}>start</button>
       <div className="grid-wrapper">
         <Grid/>
       </div>
