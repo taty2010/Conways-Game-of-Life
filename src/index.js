28fd4e265fdb93bac1e582a7e76bfee35cecfe3f
taty2010 <shydalys123@gmail.com>
2020-10-24 16:59:38 -0500

    fix?

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter  as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
   </Router>,
   document.getElementById('root')
 );
